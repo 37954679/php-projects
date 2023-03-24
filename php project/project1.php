89efa73d1b6ce7f76b1536f26290a02500bb2b51
Beatrice odera <107179654+37954679@users.noreply.github.com>
2023-03-24 15:07:18 +0300

    Add files via upload

---
+++ php project/project1.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en"dir="ltr"
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+<body>
+    <form action="functions.php"method="get">
+        <input type="text"name="num01"placeholder="Number 1">
+        <select name="oper">
+            <label>choose operation</label>
+            <option value="add"add</option>
+            <option value="sub"subtract</opton>
+</select>
+<inut type="text>name="num02"placeholder="number 2">
+<button type="submit">calculate!</button>
+</form>
+</body>
+</html>
